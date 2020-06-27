a286ac7c942b650d1807d278d81b319bfd286d29
vbodnarchuk <victor.bodnarchyk@gmail.com>
2020-06-27 23:01:38 +0300

    New neyron mechanic (not full doing)

--- first-second.h
+++ first-second.h
@@ -42,6 +42,10 @@ double ney2_3;
 double ney2_4;
 double ney2_5;
 
+double ney2;
+double ney3;
+double ney4;
+
 double ney3_1;
 double ney3_2;
 double ney3_3;
@@ -69,6 +73,21 @@ int abc(float x) {
 	if (x < 0) return x;
 	else return x * -1;
 }
+int neyron2(double n1, double n2, double n3, double n4, double n5, double n6, double n7, double n8, double n9, double n10, double n11, double n12, double n13, double n14, double w1, double w2, double w3, double w4, double w5, double w6, double w7, double w8, double w9, double w10, double w11, double w12, double w13, double w14) {
+	ney2 = n1 * w1 + n2 * w2 + n3 * w3 + n4 * w4 + n5 * w5 + n6 * w6 + n7 * w7 + n8 * w8 + n9 * w9 + n10 * w10 + n11 * w11 + n12 * w12 + n13 * w13 + n14 * w14;
+	return ney2;
+}
+
+int neyron3(double n1, double n2, double n3, double n4, double n5 , double w1, double w2, double w3, double w4, double w5) {
+	ney3 = n1 * w1 + n2 * w2 + n3 * w3 + n4 * w4 + n5 * w5;
+	return ney3;
+}
+
+int neyron4(double n1, double n2, double n3, double n4, double n5, double n6, double n7 , double w1, double w2, double w3, double w4, double w5, double w6, double w7) {
+	ney4 = n1 * w1 + n2 * w2 + n3 * w3 + n4 * w4 + n5 * w5 + n6 * w6 + n7 * w7;
+	return ney4;
+}
+
 void neyron2_1(double n1, double n2, double n3, double n4, double n5, double n6, double n7, double n8, double n9, double n10, double n11, double n12, double n13, double n14, double w1, double w2, double w3, double w4, double w5, double w6, double w7, double w8, double w9, double w10, double w11, double w12, double w13, double w14) {
 	ney2_1 = n1 * w1 /*+ n2 * w2 + n3 * w3 + n4 * w4 + n5 * w5 + n6 * w6 + n7 * w7 + n8 * w8 + n9 * w9 + n10 * w10 + n11 * w11*/;
 	ney2_1 = n2 * w2 + ney2_1;
